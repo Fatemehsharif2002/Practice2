4a27870013a117042b83893bc67dc0a34e989ea6
Fatemeh sharifmoghadam <fsharifmoghadam24@fed.idserve.net>
2024-11-04 10:55:29 +0100

    Initial Commit

---
+++ Practice2/Practice2App.swift
@@ -0,0 +1,17 @@
+//
+//  Practice2App.swift
+//  Practice2
+//
+//  Created by Fatemeh sharifmoghadam on 17/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Practice2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
